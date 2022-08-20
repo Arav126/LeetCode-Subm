0ac75dc50878b695332afa427d8400bed696f1b3
Aravind Suresh <aravind.2002.suresh@gmail.com>
2022-08-21 02:16:45 +0530

    Time: 131 ms (54.56%), Space: 15.4 MB (48.44%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -20,12 +20,44 @@ class Solution:
                 
 #         return ans
 
-        ans =[]
-        if (target not in nums):
+#         ans =[]
+#         if (target not in nums):
+#             return [-1,-1]
+        
+#         ans.append(nums.index(target))
+#         nums.sort(reverse = True)
+#         ans.append(len(nums)-nums.index(target)-1)
+        
+#         return ans
+
+        # implementation using binary search
+    
+        lo = 0
+        hi = len(nums)-1
+        
+        ind = -1
+        while(lo<=hi):
+            mi = (lo+hi)//2
+            if nums[mi] == target:
+                ind = mi
+                break
+            elif nums[mi]> target:
+                hi = mi-1
+            else:
+                lo = mi+1
+                
+        # print(ind) # this index is not the first or last occurence, but we have a position now
+        
+        if ind == -1:
             return [-1,-1]
         
-        ans.append(nums.index(target))
-        nums.sort(reverse = True)
-        ans.append(len(nums)-nums.index(target)-1)
-        
-        return ans
+        f = l = ind
+        while(1):
+            if f-1 > -1 and nums[f-1] == nums[f]:
+                f-=1
+            elif l+1 < len(nums) and nums[l+1] == nums[l]:
+                l+=1
+            else:
+                break
+                
+        return [f,l]
