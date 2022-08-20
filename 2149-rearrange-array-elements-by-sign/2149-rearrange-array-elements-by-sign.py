1e91f14eeb1de778c4b0c261ff642f7065a38fbe
Aravind Suresh <aravind.2002.suresh@gmail.com>
2022-08-20 11:50:10 +0530

    Time: 1665 ms (87.37%), Space: 44.6 MB (73.00%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -22,14 +22,26 @@ class Solution:
                     
 #             nums[]
 
-        ret = [0] * len(nums)
-        pos, neg = 0,1
-        for i in range(len(nums)):
-            if nums[i] > 0 :
-                ret[pos] = nums[i]
-                pos+=2
-            if nums[i] < 0 :
-                ret[neg] = nums[i]
-                neg+=2
+        # approach 1
+#         ret = [0] * len(nums)
+#         pos, neg = 0,1
+#         for i in range(len(nums)):
+#             if nums[i] > 0 :
+#                 ret[pos] = nums[i]
+#                 pos+=2
+#             if nums[i] < 0 :
+#                 ret[neg] = nums[i]
+#                 neg+=2
                 
+#         return ret
+        # approach 2
+        pos = [i for i in nums if i>0]
+        neg = [i for i in nums if i<0]
+        
+        ret = []
+        
+        for i in range(len(pos)):
+            ret.append(pos[i])
+            ret.append(neg[i])
+            
         return ret
