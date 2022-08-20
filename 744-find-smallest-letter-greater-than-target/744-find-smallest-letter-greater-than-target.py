0702b4abcceb5c72dff8c866425e7b72d271c757
Aravind Suresh <aravind.2002.suresh@gmail.com>
2022-08-21 02:01:26 +0530

    Time: 136 ms (76.17%), Space: 14.3 MB (62.49%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.py
@@ -3,10 +3,25 @@ class Solution:
         
         # Linear Search 
         
-        for i in letters:
-            if i > target:
-                return i
+#         for i in letters:
+#             if i > target:
+#                 return i
             
-        return letters[0] # if nothing is greater
+#         return letters[0] # if nothing is greater
             
+        # binary search
         
+        lo = 0 
+        hi = len(letters)-1
+        
+        while(lo<=hi):
+            m = (lo+hi)//2
+            if letters[m] > target:
+                hi = m-1
+            elif letters[m] <= target:
+                lo = m+1
+                
+        if hi+1 == len(letters):
+            return letters[0]
+        else:
+            return letters[hi+1] # same as bisect left
