e3a0ca3e07991abe9574c7d464e3dad0333f3b9c
Aravind Suresh <aravind.2002.suresh@gmail.com>
2022-08-20 21:46:36 +0530

    Time: 64 ms (19.06%), Space: 13.8 MB (96.38%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.py
+++ 367-valid-perfect-square/367-valid-perfect-square.py
@@ -1,10 +1,28 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         
-        x = num**0.5 # other words for a sqrt function
+#         x = num**0.5 # other words for a sqrt function
         
-        if(int(x)==x):
-            return True
-        else:
-            return False
+#         if(int(x)==x):
+#             return True
+#         else:
+#             return False
+
+        # Simple binary search with guess squared approach
+    
+        if num<2: return True
+        
+        lo = 2
+        hi = num//2
+        
+        while(lo<=hi):
+            mi = (lo+hi)//2
+            if mi**2 == num:
+                return True
+            elif mi**2 > num: # then u must move back
+                hi = mi-1
+            elif mi**2 < num:
+                lo = mi+1
+                
+        return False
         
